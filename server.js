83a4b408b3c72eb978ef2f368d1a9e283428a6c8
Mike Sowers <msowers72@yahoo.com>
2021-08-07 15:28:15 -0400

    updated code on server.js

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const { v4: uuidv4 } = require('uuid');
 const { clog } = require('./middleware/clog');
+
 
 
 const PORT = process.env.PORT || 3001;
@@ -20,37 +22,68 @@ app.use(express.static('public'));
 
 // GET Route for homepage
 app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+    res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
+app.get('/api/notes', (req, res) =>
+res.sendFile(path.join(__dirname, '/db/db.json'))
+)
+
 // POST Route
-tips.post('/', (req, res) => {
+app.post('/api/notes', (req, res) => {
     console.log(req.body);
-  
-    const { username, topic, tip } = req.body;
-  
+
+    const { title, text } = req.body;
+
     if (req.body) {
-      const newTip = {
-        username,
-        tip,
-        topic,
-        tip_id: uuidv4(),
-      };
-  
-      readAndAppend(newTip, './db/tips.json');
-      res.json(`Tip added successfully 🚀`);
+        const newNote = {
+            title,
+            text,
+            id: uuidv4(),
+        };
+
+        fs.readFile('./db/db.json', 'utf8', (err, data) => {
+            if (err) {
+                console.error(err);
+            } else {
+                // Convert string into JSON object
+                const parsedNotes = JSON.parse(data);
+
+                // Add a new review
+                parsedNotes.push(newNote);
+
+                // Write updated reviews back to the file
+                fs.writeFile(
+                    './db/db.json',
+                    JSON.stringify(parsedNotes, null, 4),
+                    (writeErr) =>
+                        writeErr
+                            ? console.error(writeErr)
+                            : console.info('Successfully updated notes!')
+                );
+            }
+        });
+
+        const response = {
+            status: 'success',
+            body: newNote,
+        };
+
+        console.log(response);
+        res.json(response);
     } else {
-      res.error('Error in adding tip');
+        res.json('Error in posting notes');
     }
-  });
+
+});
 
 
 app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT} 🚀`)
+    console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
 
 
