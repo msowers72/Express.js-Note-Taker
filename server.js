37759921e2744491a67290fc4d4c2872a83f149a
Mike Sowers <msowers72@yahoo.com>
2021-08-07 15:05:03 -0400

    updated server.js

--- server.js
+++ server.js
@@ -27,8 +27,30 @@ app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
+// POST Route
+tips.post('/', (req, res) => {
+    console.log(req.body);
+  
+    const { username, topic, tip } = req.body;
+  
+    if (req.body) {
+      const newTip = {
+        username,
+        tip,
+        topic,
+        tip_id: uuidv4(),
+      };
+  
+      readAndAppend(newTip, './db/tips.json');
+      res.json(`Tip added successfully 🚀`);
+    } else {
+      res.error('Error in adding tip');
+    }
+  });
 
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
+
+
