4e9a47553d4f2df66659c5679257e97f9c29e9ce
Mike Sowers <msowers72@yahoo.com>
2021-08-07 14:40:04 -0400

    updated server.js

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const fs = require('fs');
 const path = require('path');
 const { clog } = require('./middleware/clog');
-const api = require('./routes/index.js');
 
-const PORT = process.env.port || 3001;
+
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
@@ -13,7 +14,7 @@ app.use(clog);
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
+
 
 app.use(express.static('public'));
 
@@ -22,15 +23,11 @@ app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-// GET Route for feedback page
-app.get('/feedback', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/pages/feedback.html'))
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-// Wildcard route to direct users to a 404 page
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/pages/404.html'))
-);
+
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
